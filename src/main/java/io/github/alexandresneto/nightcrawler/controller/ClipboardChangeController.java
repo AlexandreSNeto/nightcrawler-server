7b672b4afe02d7db3e6743ef853b9643ff3d7cb9
Alexandre Silveira Neto <alexandre.sneto@gmail.com>
2021-05-05 14:10:47 -0300

    Adiciona mais logs

--- src/main/java/io/github/alexandresneto/nightcrawler/controller/ClipboardChangeController.java
+++ src/main/java/io/github/alexandresneto/nightcrawler/controller/ClipboardChangeController.java
@@ -16,18 +16,19 @@ public class ClipboardChangeController {
 
     @PostMapping
     public void register(@RequestBody ClipboardInfo info) {
-        log.info("Registrando alterações {}", info);
+        log.info("Clipboard Server :: New content register: {}", info);
 
         database.put(info.getUsername(), info);
     }
 
     @GetMapping("/{username}/{hostname}")
     public ClipboardInfo get(@PathVariable("username") String username, @PathVariable("hostname") String hostname) {
-        log.info("Buscando alterações para o usuario {} a partir do hostname {} ", username, hostname);
+        log.info("Clipboard Server :: Getting content with username '{}' from hostname '{}'", username, hostname);
 
         ClipboardInfo clipboardInfo = database.get(username);
         if (clipboardInfo != null && !clipboardInfo.getHostname().equals(hostname)) {
             database.remove(username);
+            log.info("Clipboard Server :: Content founded: {}", clipboardInfo);
             return clipboardInfo;
         }
 
